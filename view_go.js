26c95796c904aedd4abad6c0b98a9b14f59b89f2
Oh YoungTaek <robbieinertia@gmail.com>
2016-07-02 16:32:33 +0900

    6목, 3-3불허 4-3 허용

--- view_go.js
+++ view_go.js
@@ -1,11 +1,17 @@
 (function() {
+
 var view = {}
 
 
 view.add_circ = function (x,y) {
 
+    var where_id = x + 10000*y
+    model.dic_add('verify_data',{'cx':x,'cy':y,'r':2,'fill':dol_color},where_id)
+    if (validate.isThreeThree(model.call_data('verify_data'),x,y))
+        {alert('invalid'); return false}
+
     model.nosql_push('circ_data',{'cx':x,'cy':y,'r':2,'fill':dol_color})
-    model.nosql_push('list_go', x + 10000*y)
+    model.nosql_push('list_go', where_id)
 
     model.backup_data('circ_data','circ_back')
     model.backup_data('list_go','list_back')
@@ -13,6 +19,9 @@ view.add_circ = function (x,y) {
     dol_color = dol_color == 'black' ? 'white' : 'black' 
 
     view.refresh() 
+
+    if( validate.isWin(verify_data,x,y))
+       {alert('win')}
 
 }
 
@@ -22,11 +31,15 @@ view.remove_circ = function (all) {
     {
         model.init_list('circ_data')
         model.init_list('list_go')
+        model.init_obj('verify_data')
     }
     else
     {
        model.pop('circ_data') //마지막만 제거
+       var get_last_id = model.call_data('list_go')
+       get_last_id = get_last_id[get_last_id.length-1] 
        model.pop('list_go')
+       model.pop_obj('verify_data',get_last_id)
     }
 
     dol_color = dol_color == 'black' ? 'white' : 'black' 
@@ -36,7 +49,8 @@ view.remove_circ = function (all) {
 
 view.refresh = function() {
 
-    var selection =  d3.select('svg').selectAll('circle').data(model.call_data('circ_data'))
+    var call_d = model.call_data('circ_data')
+    var selection =  d3.select('svg').selectAll('circle').data(call_d)
 
     selection.enter().append('circle')
             .attr('cx',function(d){return d.cx})
@@ -52,6 +66,7 @@ view.refresh = function() {
 //백업
 view.recover = function () {
 
+    console.log('recover')
     model.recover_data('circ_data','circ_back')
     model.recover_data('list_go','list_back')
 
